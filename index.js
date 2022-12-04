31402021a33a3f002e34e3c9a99da9031bd81ce1
Brittni <brittni.toporek@gmail.com>
2022-12-04 16:36:19 -0700

    updated js file

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ A Bootstrap styled table representing your choice of data.
 A Bootstrap styled form that allows a user to add a new row to the table when clicking on submit.
 
 Dnd Party Generator:
-Table to include: name, race, class, alignment; example listed in first row
+Table to include: name, race, class, alignment; example listed in first row?
 Functions to include
 -add member and party (use class)
 -buttons to create and delete
